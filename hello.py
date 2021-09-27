c55f4411f8ba5e67f9b52ff8414e9c328e8fd23e
sepl99 <seplett@gmail.com>
2021-09-27 17:35:45 -0400

    Activity 2-2 hello.py new line correction

--- hello.py
+++ hello.py
@@ -7,7 +7,8 @@ def index():
     return '<h1>Hello World!</h1>'
 
 
-@app.route('/user/<name>') def user(name):
+@app.route('/user/<name>')
+def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
 
