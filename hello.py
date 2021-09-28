c396f733b02aa12315560815160ff0164c49956f
sepl99 <seplett@gmail.com>
2021-09-28 08:45:29 -0400

    Implemented Activity 4 Step 2

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -14,6 +14,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your email?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,12 +22,18 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
-
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your name!')
+        session['email'] = form.email.data
+        session['is_uoft_email'] = True
+        if session.get('email') is not None and 'utoronto' not in session.get('email'):
+            session['is_uoft_email'] = False
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), is_uoft=session.get('is_uoft_email'))
 
 
 @app.route('/user/<name>')
